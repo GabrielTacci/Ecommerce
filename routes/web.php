1faf51192c6f89d311b95d8af32ebbc3b5ecd9aa
GabrielTacci <gabriel.htacci@gmail.com>
2024-08-27 18:02:09 -0300

    Descricao das mudancas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\ClienteController;
 
-Route::get('/', function () {
-    return view('home');
-});
+
+Route::match(['get', 'post'], '/', [ ProdutoController::class, 'index'])->name('home');
+Route::match(['get', 'post'], '/categoria', [ ProdutoController::class, 'categoria'])->name('categoria');
+Route::match(['get', 'post'], '/cadastrar', [ ClienteController::class, 'cadastrar'])->name('cadastrar');
