a19ba944c960260e47217c8241e5e740f1ca784c
GabrielTacci <gabriel.htacci@gmail.com>
2024-08-29 16:36:47 -0300

    Atualizando os layouts

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -19,26 +19,56 @@ class ProdutoController extends Controller
         return view("home", $data);
     }
 
-    public function categoria($idcategoria = 0, Request $request){
-        $data = [];
+    public function categoria(Request $request, $idcategoria = null)
+{
+    $data = [];
 
-        //Select * from categoria;
-        $listaCategorias = Categoria::all();
+    // Busca todas as categorias
+    $listaCategorias = Categoria::all();
 
-        //Select * from produtos limit 4
-        $queryProduto = Produto::limit(4);
+    // Configura a consulta para buscar produtos, limitado a 4
+    $queryProduto = Produto::limit(4);
 
-        if($idcategoria != 0){
-            //WHERE categoria_id = $idcategoria
-            $queryProduto->where("categoria_id", $idcategoria);
-        }
+    // Verifica se um id de categoria foi fornecido
+    if ($idcategoria) {
+        // Filtra os produtos pela categoria fornecida
+        $queryProduto->where("categoria_id", $idcategoria);
+    }
 
-        $listaProdutos = $queryProduto->get();
+    $listaProdutos = $queryProduto->get();
 
-        $data["lista"] = $listaProdutos;
-        $data["listaCategoria"] = $listaCategorias;
-        return view("categoria", $data);
+    $data["lista"] = $listaProdutos;
+    $data["listaCategoria"] = $listaCategorias;
+    $data["idcategoria"] = $idcategoria;
+
+    return view("categoria", $data);
+}
+
+public function adicionarCarrinho($idProduto = 0, Request $request){
+
+    //Buscar produto pelo ID
+    $prod = Produto::find($idProduto);
+
+    if($prod){
+        //Encontrou um produto
+        //Buscar da sessão o carrinho atual
+        $carrinho = session('cart', []);
+
+        array_push($carrinho, $prod);
+        session(['cart'=>$carrinho]);
+    }
+    return redirect()->route("home");
+}
+
+    public function verCarrinho(Request $request){
+        $carrinho = session('cart',[]);
+
+        dd($carrinho);
 
     }
 
+    
+
+
+    
 }
