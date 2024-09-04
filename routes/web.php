efaa35928e4c5788212097d1246f3e606be4b121
GabrielTacci <gabriel.htacci@gmail.com>
2024-09-04 15:28:16 -0300

    Criacao da tela de Login e o UsuarioController.

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProdutoController;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\UsuarioController;
+
 
 
 Route::match(['get', 'post'], '/', [ ProdutoController::class, 'index'])->name('home');
@@ -10,6 +12,7 @@ Route::match(['get', 'post'], '/categoria/{idcategoria}', [ProdutoController::cl
 Route::match(['get', 'post'], '/categoria', [ProdutoController::class, 'categoria'])->name('categoria');
 Route::match(['get', 'post'], '/cadastrar', [ ClienteController::class, 'cadastrar'])->name('cadastrar');
 Route::match(['get', 'post'], '/cliente/cadastrar', [ ClienteController::class, 'cadastrarCliente'])->name('cadastrar_cliente');
+Route::match(['get', 'post'], '/logar', [ UsuarioController::class, 'logar'])->name('logar');
 Route::match(['get', 'post'], '/{idproduto}/carrinho/adicionar', [ ProdutoController::class, 'adicionarCarrinho'])->name('adicionar_carrinho');
 Route::match(['get', 'post'], '/carrinho', [ ProdutoController::class, 'verCarrinho'])->name('ver_carrinho');
 Route::match(['get', 'post'], '/{indice}/excluircarrinho', [ ProdutoController::class, 'excluirCarrinho'])->name('excluir_carrinho');
