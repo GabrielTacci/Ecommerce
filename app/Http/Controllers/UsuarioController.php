88093e399adbe8eea70a1c7035741e74cb9ebc6c
GabrielTacci <gabriel.htacci@gmail.com>
2024-09-05 15:03:25 -0300

    Melhorias no sistema de login.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -3,11 +3,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
 
 class UsuarioController extends Controller
 {
     public function logar(Request $request){
         $data = [];
+
+        if($request->isMethod("POST")){
+
+            $login = $request->input("login");
+            $senha = $request->input("senha");
+
+            $credential = ['login' => $login, 'password' => $senha ];
+
+            if(Auth::attempt($credential)){
+                return redirect()->route("home");
+            }else{
+                $request->session()->flash("erro", "Usuario ou senha inválido");
+                return redirect()->route("logar");
+            }
+        }
 
         return  view("logar", $data);
     }
--- app/Models/Usuario.php
+++ app/Models/Usuario.php
@@ -1,9 +1,39 @@
 <?php
 
 namespace App\Models;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Auth\Authenticatable;
 
-class Usuario extends RModel
+class Usuario extends RModel implements AuthenticatableContract
 {
+    use Authenticatable;
+
     protected $table = "usuarios";
+
     protected $fillable =['email','login', 'password', 'nome'];
+
+    public function getAuthIdentifierName(){
+        return $this->getKey();
+    }
+
+    public function getAuthIdentifier(){
+        return $this->login;
+
+    }
+    public function getAuthPassword(){
+        return $this->password;
+    }
+
+    public function getRememberToken(){
+        return $this->remember_token; 
+    }
+
+    public function setRememberToken($value){
+        $this->remember_token = $value; 
+    }
+
+    public function getRememberTokenName(){
+        return 'remember_token'; 
+
+    }
 }
